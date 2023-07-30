52211517b1ad81a4f46a215f8722ba3c06a0bcc5
Tiago Costa <tiagodpv@gmail.com>
2023-07-30 00:40:10 -0400

    fith

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-inteiros = [1,3,4,5,7,8,9]
-pares = [x for x in inteiros if x % 2 == 0]
+def soma_dois_numeros(primeiro_numero, segundo_numero):
+    print(primeiro_numero + segundo_numero)
 
+def subtrai_dois_numeros{primeiro_numero, segundo_numero}
+    print(primeiro_numero - segundo_numero)
 
-print(pares)
+def multiplica_dois_numeros(primeiro_numero, segundo_numero):
+    print(primeiro_numero * segundo_numero)
+
+def divide_dois_numeros(primeiro_numero, segundo_numero):
+    print(primeiro_numero / segundo_numero)
