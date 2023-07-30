12258ac9a87a738a4464f45029a541b6ae2a3429
Tiago Costa <tiagodpv@gmail.com>
2023-07-30 01:02:33 -0400

    sev

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 def soma_dois_numeros(primeiro_numero, segundo_numero):
     print(primeiro_numero + segundo_numero)
 
-def subtrai_dois_numeros{primeiro_numero, segundo_numero}
-    print(primeiro_numero - segundo_numero)
 
 def multiplica_dois_numeros(primeiro_numero, segundo_numero):
     print(primeiro_numero * segundo_numero)
